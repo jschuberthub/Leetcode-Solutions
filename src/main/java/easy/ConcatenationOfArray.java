87704027f8bbecf7965450d0ae66d55738fc89b8
Julian Schubert <it0-schuju@student.itech-bs14.de>
2022-01-16 17:37:36 +0100

    Solved Concatenation of Array / 1929

---
+++ src/main/java/easy/ConcatenationOfArray.java
@@ -0,0 +1,13 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ConcatenationOfArray {
+
+//  https://leetcode.com/problems/concatenation-of-array/submissions/
+    public int[] getConcatenation(int[] nums) {
+        return IntStream.concat(Arrays.stream(nums), Arrays.stream(nums)).toArray();
+    }
+
+}
