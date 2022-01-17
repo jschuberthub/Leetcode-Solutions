c20bddfe18b843a52b35363cfb078151bec7c01c
Julian Schubert <it0-schuju@student.itech-bs14.de>
2022-01-17 22:24:38 +0100

    Solved Richest Customer Wealth / 1672

---
+++ src/main/java/easy/RichestCustomerWealth.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class RichestCustomerWealth {
+
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int sum = 0;
+        for (int[] account : accounts) {
+            for (int j = 0; j < accounts[0].length; j++) {
+                sum += account[j];
+            }
+            if (sum > max) max = sum;
+            sum = 0;
+        }
+        return max;
+    }
+}
