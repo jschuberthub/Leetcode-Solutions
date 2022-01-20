e59779ac79fdb2109a6dc84e2a2e9c5cf328e595
Julian Schubert <it0-schuju@student.itech-bs14.de>
2022-01-20 15:29:37 +0100

    Solved Jewels and Stones / 771

---
+++ src/main/java/easy/JewelsAndStones.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class JewelsAndStones {
+
+  public int numJewelsInStones(String jewels, String stones) {
+    int result = 0;
+    for (int i = 0; i < stones.length(); i++) {
+      if (jewels.indexOf(stones.charAt(i)) != -1) {
+        result++;
+      }
+    }
+    return result;
+  }
+}
