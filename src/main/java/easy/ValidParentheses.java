f0fb00c7eda3395408769469ee90bf837caf6f0b
Julian Schubert <it0-schuju@student.itech-bs14.de>
2022-01-30 14:15:48 +0100

    Solved Two Sum and Valid Parentheses

---
+++ src/main/java/easy/ValidParentheses.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class ValidParentheses {
+
+  public boolean isValid(String s) {
+    int length;
+
+    do {
+      length = s.length();
+      s = s.replace("()", "").replace("{}", "").replace("[]", "");
+    } while (length != s.length());
+
+    return s.length() == 0;
+  }
+}
