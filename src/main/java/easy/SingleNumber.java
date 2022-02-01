8a932266d736f95cfb7e69bb7af9a7c3157f2d60
Julian Schubert <it0-schuju@student.itech-bs14.de>
2022-02-01 21:06:10 +0100

    Solved Single Number / 136

---
+++ src/main/java/easy/SingleNumber.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class SingleNumber {
+
+  public int singleNumber(int[] nums) {
+    int result = 0;
+    for (int i : nums) {
+      result ^= i;
+    }
+    return result;
+  }
+}
