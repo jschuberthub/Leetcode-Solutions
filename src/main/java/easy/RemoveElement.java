57fefdc9791728070ae985ea32fb8c803df87978
Julian Schubert <it0-schuju@student.itech-bs14.de>
2022-02-01 21:01:55 +0100

    Solved Remove Element / 27

---
+++ src/main/java/easy/RemoveElement.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class RemoveElement {
+
+  public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int n : nums) if (n != val) nums[i++] = n;
+    return i;
+  }
+}
